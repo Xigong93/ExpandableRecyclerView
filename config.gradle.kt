53ecab0743d58569c4a13afa1ea09fe6c426abf9
pokercc <pokercc@sina.com>
2020-07-10 20:10:34 +0800

    min api 改为19

--- config.gradle.kt
+++ config.gradle.kt
@@ -3,7 +3,7 @@ android {
     buildToolsVersion "29.0.3"
 
     defaultConfig {
-        minSdkVersion 21
+        minSdkVersion 19
         targetSdkVersion 29
         versionCode 1
         versionName "1.0"
